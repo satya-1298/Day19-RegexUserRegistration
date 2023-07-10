227467ffbe0e808285ef6ae2248a45ee189289be
unknown <satya-1298>
2023-07-10 17:45:41 +0530

    [Satya]Add : Validating first name based on Regular Expression

---
+++ UserRegistration/Details.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Details
+    {
+        public void FirstNameValidate()
+        {
+            Console.WriteLine("Enter the First name ");
+            string name = Console.ReadLine();
+            string firstname = "^[A-Z]{1}[a-z]{3,}?";
+            if (Regex.IsMatch(name, firstname))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is INVALID");
+            }
+        }
+    }
+}
---
+++ UserRegistration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Details details = new Details();
+                Console.WriteLine("*****************");
+                Console.WriteLine("-Validate first name");
+                Console.WriteLine("*****************");
+                Console.WriteLine("Choose the Above options");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+
+                    case 1:
+                        details.FirstNameValidate();
+                        Console.WriteLine("*****************");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option");
+                        break;
+
+                }
+            }
+        }
+    }
+}
